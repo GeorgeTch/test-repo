3154a6d5b2bb12c3accfdfb30bc3cd09eb5617a6
George <georgetchintcharauli@gmail.com>
2024-03-10 23:49:49 +0400

    Updates for session 2 homework

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <section>
+      <h2>Welcome to Our Landing Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id justo
+        vitae ex consequat dapibus eu eget orci.
+      </p>
+      <button>
+        <a href="https://react.dev/">Learn More</a>
+      </button>
+    </section>
+  );
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://termify.io/terms-and-conditions-generator?gad_source=1&gclid=CjwKCAiA0bWvBhBjEiwAtEsoWzs2jSqzrLqZpJmdtm---oXR8xYTOLYo0L-QaQeKJSFxdm-dFJmo8RoCY9cQAvD_BwE">
+          Terms & Conditions
+        </a>
+        <a href="https://fsymbols.com/copyright/">Privacy Policy</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
