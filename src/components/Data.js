6714551a1b26801dd903a2d8188bed783a0aab9a
George <georgetchintcharauli@gmail.com>
2024-03-25 02:37:14 +0400

    extracted data from the component and imported from a new file

---
+++ src/components/Data.js
@@ -0,0 +1,57 @@
+const productsData = [
+  {
+    title: "Jabra Elite 8 Active",
+    description:
+      "Best and Most Advanced Sports Wireless Bluetooth Earbuds with Comfortable Secure Fit",
+    image:
+      "https://m.media-amazon.com/images/I/512x-48508L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "JBL Tune Flex",
+    description: "True Wireless Noise Cancelling Earbuds (Black), Small",
+    image:
+      "https://m.media-amazon.com/images/I/61joErBxERL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Sony WH-1000XM5",
+    description:
+      "The Best Wireless Noise Canceling Headphones with Auto Noise Canceling Optimizer",
+    image:
+      "https://m.media-amazon.com/images/I/61vJtKbAssL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "JBL Quantum 910",
+    description: "Wireless Gaming Headset, Black, Large",
+    image:
+      "https://m.media-amazon.com/images/I/51JCUUfvo4L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+
+  {
+    title: "Jabra Elite 8 Active",
+    description:
+      "Best and Most Advanced Sports Wireless Bluetooth Earbuds with Comfortable Secure Fit",
+    image:
+      "https://m.media-amazon.com/images/I/512x-48508L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "JBL Tune Flex",
+    description: "True Wireless Noise Cancelling Earbuds (Black), Small",
+    image:
+      "https://m.media-amazon.com/images/I/61joErBxERL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Sony WH-1000XM5",
+    description:
+      "The Best Wireless Noise Canceling Headphones with Auto Noise Canceling Optimizer",
+    image:
+      "https://m.media-amazon.com/images/I/61vJtKbAssL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "JBL Quantum 910",
+    description: "Wireless Gaming Headset, Black, Large",
+    image:
+      "https://m.media-amazon.com/images/I/51JCUUfvo4L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+];
+
+export default productsData;
--- src/components/ProductsGallery.jsx
+++ src/components/ProductsGallery.jsx
@@ -1,3 +1,5 @@
+import productsData from "./Data";
+
 function Product({ title, description, image }) {
   return (
     <div className="product">
@@ -12,35 +14,6 @@ function Product({ title, description, image }) {
 }
 
 export default function ProductsGallery() {
-  const productsData = [
-    {
-      title: "Jabra Elite 8 Active",
-      description:
-        "Best and Most Advanced Sports Wireless Bluetooth Earbuds with Comfortable Secure Fit",
-      image:
-        "https://m.media-amazon.com/images/I/512x-48508L._AC_UY327_FMwebp_QL65_.jpg",
-    },
-    {
-      title: "JBL Tune Flex",
-      description: "True Wireless Noise Cancelling Earbuds (Black), Small",
-      image:
-        "https://m.media-amazon.com/images/I/61joErBxERL._AC_UY327_FMwebp_QL65_.jpg",
-    },
-    {
-      title: "Sony WH-1000XM5",
-      description:
-        "The Best Wireless Noise Canceling Headphones with Auto Noise Canceling Optimizer",
-      image:
-        "https://m.media-amazon.com/images/I/61vJtKbAssL._AC_UY327_FMwebp_QL65_.jpg",
-    },
-    {
-      title: "JBL Quantum 910",
-      description: "Wireless Gaming Headset, Black, Large",
-      image:
-        "https://m.media-amazon.com/images/I/51JCUUfvo4L._AC_UY327_FMwebp_QL65_.jpg",
-    },
-  ];
-
   return (
     <div className="products-container">
       {productsData.map((product, idx) => (
